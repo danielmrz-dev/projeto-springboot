5c5481b591eba426e32f5c52bc50b167573efb12
Daniel Moraes Mariz <danielmrz.dev@gmail.com>
2025-02-19 11:30:43 -0300

    Cria models

--- src/main/java/api/rest/Application.java
+++ src/main/java/api/rest/Application.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjetoSpringbootApplication {
+public class Application {
 
 	public static void main(String[] args) {
 		
-		SpringApplication.run(ProjetoSpringbootApplication.class, args);
+		SpringApplication.run(Application.class, args);
 		System.out.println("Projeto rodando!");
 	}
 
