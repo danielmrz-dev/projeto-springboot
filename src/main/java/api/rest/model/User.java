95045f381e3f1cb6e2d8725d938e4b706d7e053e
Daniel Moraes Mariz <danielmrz.dev@gmail.com>
2025-02-19 13:36:30 -0300

    Finalizado

--- src/main/java/api/rest/model/User.java
+++ src/main/java/api/rest/model/User.java
@@ -7,6 +7,8 @@ import java.util.List;
 @Entity(name = "tb_user")
 public class User {
 
+    public User() {}
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
